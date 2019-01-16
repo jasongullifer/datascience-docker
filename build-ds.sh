4d0f1a1f5ae39e3a68c2d3d7c559e3f395195b89
Jason W. Gullifer <jason.gullifer@gmail.com>
2019-01-16 11:42:43 -0500

    fix line endings

--- build-ds.sh
+++ build-ds.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
-docker build -t jg-ds-python -f ./ds-python/DockerFile ./
-docker build -t jg-ds-r      -f ./ds-r/DockerFile      ./
-
+#!/bin/bash
+docker build -t jg-ds-python -f "./ds-python/DockerFile" ./
+docker build -t jg-ds-r      -f "./ds-r/DockerFile"      ./
 docker-compose up
