c8484844a97cd0cc39f8c4d0645117c5b1f0af05
Jason W. Gullifer <jason.gullifer@gmail.com>
2019-01-16 16:53:35 -0500

    add packages

--- build-ds.sh
+++ build-ds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t jg-ds-python -f "./ds-python/DockerFile" ./
-docker build -t jg-ds-r      -f "./ds-r/DockerFile"      ./
-docker-compose up
+docker build -t jg-ds-python -f "./ds-python/DockerFile" .
+docker build -t jg-ds-r      -f "./ds-r/DockerFile"      .
+docker-compose up 
